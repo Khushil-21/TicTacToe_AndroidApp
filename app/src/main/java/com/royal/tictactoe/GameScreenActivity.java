1c3887c45d56a56c0ab435c12f0158b23784a1e3
Khushil-21 <khushilshah2105@gmail.com>
2025-02-25 23:20:08 +0530

    action bar / light mode / app logo and name

--- app/src/main/java/com/royal/tictactoe/GameScreenActivity.java
+++ app/src/main/java/com/royal/tictactoe/GameScreenActivity.java
@@ -1,9 +1,12 @@
 package com.royal.tictactoe;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -13,6 +16,20 @@ public class GameScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Remove action bar
+        getSupportActionBar().hide();
+
+        // force to open app in light mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        // change action bar color after super.onCreate()
+//        androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#fffffc"))); //Orange
+//        }
+
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
